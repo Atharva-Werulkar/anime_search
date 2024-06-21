11f95eab9017e10a84637b9255b81b97f038abe7
Atharva Werulkar <werulkaratharva@gmail.com>
2024-06-21 18:11:06 +0530

    Update

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xFF1c1d1f);
-const Color secondaryColor = Color(0xFF3d3f43);
+const Color secondaryColor = Color.fromARGB(255, 44, 45, 47);
 const Color textColor = Colors.white;
 const Color primaryIconColor = Color(0xFF13bd50);
 const Color secondaryIconColor = Color(0xFF8b8b8d);
